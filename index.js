5ab134e8873d0c3ba100b709b4a66d281f89eeec
Brett Kuhlman <brett.kuhlman@proton.me>
2025-11-02 19:59:36 -0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Test Package
+ * A simple test package for GitHub Packages demonstration
+ */
+
+function greet(name) {
+  return 'Hello, ' + name + '!';
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = {
+  greet,
+  add
+};
